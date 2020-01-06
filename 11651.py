2e2204a191187610e2bb089e61b9087183566c66
Gadon <jun752601@gmail.com>
2020-01-06 16:37:20 +0900

    11651_좌표 정렬하기 2
    https://www.acmicpc.net/problem/11650

---
+++ 11651.py
@@ -0,0 +1,15 @@
+#11651_좌표 정렬하기 2
+#https://www.acmicpc.net/problem/11650
+import sys
+def key2(a):
+    return a[1]
+n = int(input())
+f = []
+for i in range(n):
+    x,y = (list(map(int,sys.stdin.readline().rstrip().split())))
+    f.append((x,y))
+
+f.sort()
+f.sort(key = key2)
+for i in range(n):
+    print("%d %d\n"%(f[i][0],f[i][1]), end='')
